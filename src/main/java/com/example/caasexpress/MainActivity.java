d1acd74695f5cd7dd6e34f7b70c11b0ae6636f66
Soumak Majumdar <soumakmajumdar904@gmail.com>
2019-04-01 05:15:50 +0530

    CAAS Express Updated
    Signed-off-by: Soumak Majumdar <soumakmajumdar904@gmail.com>

--- src/main/java/com/example/caasexpress/MainActivity.java
+++ src/main/java/com/example/caasexpress/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.caasexpress;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signupbutton, loginbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signupbutton = (Button) findViewById(R.id.main_signup_bttn);
+        loginbutton = (Button) findViewById(R.id.main_login_bttn);
     }
+
+
 }
