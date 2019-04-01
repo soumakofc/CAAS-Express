27201fd5d9a8ff0713f07030051dbddba46c6608
Soumak Majumdar <soumakmajumdar904@gmail.com>
2019-04-01 19:11:56 +0530

    Login Page added
    Signed-off-by: Soumak Majumdar <soumakmajumdar904@gmail.com>

--- src/main/java/com/example/caasexpress/MainActivity.java
+++ src/main/java/com/example/caasexpress/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.caasexpress;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button signupbutton, loginbutton;
+    public static final String EXTRA_MESSAGE = "com.example.caasexpress";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
         loginbutton = (Button) findViewById(R.id.main_login_bttn);
     }
 
+    public void OnClickLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, "Login Page!");
+        startActivity(intent);
+    }
 
 }
